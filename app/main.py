bfc2a14f98359974d671d43191db65872132a31b
Sebastian Pacurar <pacurar.sebastian90@gmail.com>
2024-02-04 01:19:14 +0200

    add AppBar back functionality

--- app/main.py
+++ app/main.py
@@ -37,6 +37,7 @@ class MyKivyApp(MDApp):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
         self.data = None
+        self.prev_screen = None
         self.db = Database()
 
     def build(self):
@@ -54,16 +55,38 @@ class MyKivyApp(MDApp):
                 "on_release", lambda x: self.change_to_add_screen(x)
             ],
         }
+        self.prev_screen = self.root.ids.scr_manager.current_screen.name
+        self.update_top_bar()
         self.root.ids.speed_dial.data = self.data
         self.get_lists()
 
     def change_to_add_screen(self, option):
         self.root.ids.speed_dial.close_stack()
-        sm = self.root.ids.scr_manager
         if option.icon == 'food-drumstick':
-            sm.current = 'add_prod_scr'
+            self.change_screen('add_prod_scr')
         else:
-            sm.current = 'add_list_scr'
+            self.change_screen('add_list_scr')
+
+    def update_top_bar(self):
+        top_bar = self.root.ids.top_bar
+        sm = self.root.ids.scr_manager
+        nav_drawer = self.root.ids.nav_drawer
+        if sm.current in ['prod_scr', 'list_scr']:
+            top_bar.left_action_items = [['menu', lambda _: nav_drawer.set_state('open')]]
+        else:
+            top_bar.left_action_items = [['arrow-left', lambda _: self.navigate_back()]]
+
+    def navigate_back(self):
+        sm = self.root.ids.scr_manager
+        sm.current = self.prev_screen
+        sm.transition.direction = 'right'
+        self.update_top_bar()
+
+    def change_screen(self, screen_name):
+        sm = self.root.ids.scr_manager
+        self.prev_screen = sm.current_screen.name
+        sm.transition.direction = 'left'
+        sm.current = screen_name
 
     # issue here
     def get_products(self):
@@ -103,18 +126,17 @@ class MyKivyApp(MDApp):
             show_list_items.add_widget(item)
 
     def get_list(self, *args):
-        try:
-            list_id = args[0].id.split('_')[1]
-            data = self.db.get_shop_list(list_id)
-            sm = self.root.ids.scr_manager
-            sm.current = 'list_content_scr'
-            list_content_items = self.root.ids.list_content_items
-            list_content_items.clear_widgets()  # Clear previous list items
-            for entry in data:
-                item = SwipeToDeleteItem(text=entry[1])
-                list_content_items.add_widget(item)
-        except Exception as e:
-            print(f"Error in get_list: {str(e)}")
+        list_id = args[0].id.split('_')[1]
+        data = self.db.get_shop_list(list_id)
+        self.change_screen('list_content_scr')
+
+        list_content_items = self.root.ids.list_content_items
+        list_content_items.clear_widgets()  # Clear previous list items
+        for entry in data:
+            item = SwipeToDeleteItem(text=entry[1])
+            list_content_items.add_widget(item)
+
+        self.update_top_bar()
 
 
 if __name__ == '__main__':
